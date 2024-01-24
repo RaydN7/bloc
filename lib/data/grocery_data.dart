da496ed07657e0839da25efecc76130e1b538be3
RaydN <87033472+RaydN7@users.noreply.github.com>
2024-01-25 00:06:45 +0530

    first commit

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,35 @@
+class GroceryData{
+
+ static List<Map<String, dynamic>> groceryProducts = [
+  {
+    'id': '1',
+    'name': 'Bananas',
+    'description': 'Fresh bananas',
+    'price': 0.49,
+    'imageUrl': 'https://i.pinimg.com/564x/1c/16/62/1c1662f546cc85a1d77732c840ff9113.jpg',
+  },
+  {
+    'id': '2',
+    'name': 'Grapes',
+    'description': 'Organic, locally grown apples',
+    'price': 1.99,
+    'imageUrl': 'https://i.pinimg.com/564x/f4/64/eb/f464eb3327d44ed66e587a726098c3fe.jpg',
+  },
+  {
+    'id': '3',
+    'name': 'Bread',
+    'description': 'Brown Bread',
+    'price': 2.49,
+    'imageUrl': 'https://i.pinimg.com/564x/46/08/b8/4608b85d26800dc031df4a319477052d.jpg',
+  },
+  {
+    'id': '4',
+    'name': 'Brown Eggs',
+    'description': 'Farm-fresh brown eggs',
+    'price': 3.99,
+    'imageUrl': 'https://i.pinimg.com/564x/7f/97/ca/7f97cab8e496df881bf14ba061047649.jpg',
+  },
+ 
+];
+
+}
